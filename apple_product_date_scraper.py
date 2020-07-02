dcde61238620d55d7d990f051e3e3e06dd4b35ef
Kevin <kloklee888@gmail.com>
2020-07-02 17:46:30 -0400

    Cleaned up code

--- apple_product_date_scraper.py
+++ apple_product_date_scraper.py
@@ -43,15 +43,6 @@ delay_long = 2 # in seconds
 ###################
 
 records = []
-start_time = datetime.now()
-
-#Translate webpages to English
-options = Options()
-prefs = {
-  "translate_whitelists": {"fr":"en", "de":"en" ,"ja":"en", "zh-CN":"en"},
-  "translate":{"enabled":"true"}
-}
-options.add_experimental_option("prefs", prefs)
 
 def save_record(color, size, country, carrier, date, date_format):
     if country != 'US':
@@ -77,132 +68,127 @@ def save_record(color, size, country, carrier, date, date_format):
     print(date_converted)
     records.append(Record(color, size, country, carrier, date_converted, day_to_ship))
 
-try:
-    #Loop through countries
-    for country_url in countries:
-        country = country_url.country
-        date_format = country_url.date_format
-        print(f'Country: {country}')
+def before_color_loop(driver, country):
+    if country == 'Canada' or country == 'Germany' or country == 'China':
+        #Click "No" for trade in
+        driver.find_element_by_xpath('//*[@id="tradeup-inline-app"]/div/div/fieldset/div/div[2]/div/div').click()
+        time.sleep(delay_long)
 
-        # Using Selenium to select dynamic content
-        driver = webdriver.Chrome(chrome_options=options)
-        driver.get(country_url.url)
-        time.sleep(delay)
+def click_element(driver, xpath):
+    element = driver.find_element_by_xpath(xpath)
+    driver.execute_script("arguments[0].scrollIntoView();", element)
+    element.click()
+    time.sleep(delay)
 
-        #Delete overlap header causing problems
-        header_element = driver.find_element_by_xpath('//*[@id="page"]/div[4]')
-        driver.execute_script("""var element = arguments[0];element.parentNode.removeChild(element);""", header_element)
+def execute_scrape(countries):
+    start_time = datetime.now()
 
-        color_length = len(driver.find_elements_by_xpath('//*[@id="Item1"]/div/fieldset/div[1]/*'))
-        size_length = len(driver.find_elements_by_xpath('//*[@id="Item2"]/div/fieldset/div/*'))
-        print(f'Color choices: {color_length}')
-        print(f'Size choices: {size_length}')
+    #Translate webpages to English
+    options = Options()
+    prefs = {
+    "translate_whitelists": {"fr":"en", "de":"en" ,"ja":"en", "zh-CN":"en"},
+    "translate":{"enabled":"true"}
+    }
+    options.add_experimental_option("prefs", prefs)
 
-        #Country specific actions
-        if country == 'US':
-            carrier_length = len(driver.find_elements_by_xpath('//*[@id="Item3"]/div/fieldset/div[1]/div[2]/*'))
-            print(f'Carrier choices: {carrier_length}')
-        
-        if country == 'Canada' or country == 'Germany' or country == 'China':
-            #Click "No" for trade in
-            driver.find_element_by_xpath('//*[@id="tradeup-inline-app"]/div/div/fieldset/div/div[2]/div/div').click()
-            time.sleep(delay_long)
+    try:
+        #Loop through countries
+        for country_url in countries:
+            country = country_url.country
+            date_format = country_url.date_format
+            print(f'Country: {country}')
 
-        #Loop through color options
-        for i in range(color_length):
-            color_element = driver.find_element_by_xpath('//*[@id="Item1"]/div/fieldset/div[1]/div['+ str(i+1) +']')
-            driver.execute_script("arguments[0].scrollIntoView(); window.scrollBy(0, -window.innerHeight);", color_element)
-            color_element.click()
+            # Using Selenium to select dynamic content
+            driver = webdriver.Chrome(chrome_options=options)
+            driver.get(country_url.url)
             time.sleep(delay)
-            color = driver.find_element_by_xpath('//*[@id="Item1"]/div/fieldset/div[1]/div['+ str(i+1) +']/div/div/label/span[2]').text
-            print(color)
-            #Loop through GB size options
-            for j in range(size_length):
-                size_element = driver.find_element_by_xpath('//*[@id="Item2"]/div/fieldset/div/div['+ str(j+1) +']')
-                driver.execute_script("arguments[0].scrollIntoView(); window.scrollBy(0, -window.innerHeight);", size_element)
-                size_element.click()
-                time.sleep(delay)
-                size = driver.find_element_by_xpath('//*[@id="Item2"]/div/fieldset/div/div['+ str(j+1) +']/div/label/span[1]').text
-                size = size.replace('*', '')
-                print(size)
-                #Loop through carrier options
-                if country == 'US':
-                    need_trade_pay = True
-                    for k in range(carrier_length):
-                        carrier_element = driver.find_element_by_xpath('//*[@id="Item3"]/div/fieldset/div[1]/div[2]/div['+ str(k+1) +']')
-                        driver.execute_script("arguments[0].scrollIntoView(); window.scrollBy(0, -window.innerHeight);", carrier_element)
-                        carrier_element.click()
-                        time.sleep(delay)
-                        carrier = driver.find_element_by_xpath('//*[@id="Item3"]/div/fieldset/div[1]/div[2]/div['+ str(k+1) +']/div/label/span[2]').text
-                        print(carrier)
+
+            #Delete overlap header causing problems
+            header_element = driver.find_element_by_xpath('//*[@id="page"]/div[4]')
+            driver.execute_script("""var element = arguments[0];element.parentNode.removeChild(element);""", header_element)
+
+            color_length = len(driver.find_elements_by_xpath('//*[@id="Item1"]/div/fieldset/div[1]/*'))
+            size_length = len(driver.find_elements_by_xpath('//*[@id="Item2"]/div/fieldset/div/*'))
+            print(f'Color choices: {color_length}')
+            print(f'Size choices: {size_length}')
+            if country == 'US':
+                carrier_length = len(driver.find_elements_by_xpath('//*[@id="Item3"]/div/fieldset/div[1]/div[2]/*'))
+                print(f'Carrier choices: {carrier_length}')
+
+            before_color_loop(driver, country)
+
+            #Loop through color options
+            for i in range(color_length):
+                click_element(driver, '//*[@id="Item1"]/div/fieldset/div[1]/div['+ str(i+1) +']')
+                color = driver.find_element_by_xpath('//*[@id="Item1"]/div/fieldset/div[1]/div['+ str(i+1) +']/div/div/label/span[2]').text
+                print(color)
+                #Loop through GB size options
+                for j in range(size_length):
+                    click_element(driver, '//*[@id="Item2"]/div/fieldset/div/div['+ str(j+1) +']')
+                    size = driver.find_element_by_xpath('//*[@id="Item2"]/div/fieldset/div/div['+ str(j+1) +']/div/label/span[1]').text
+                    size = size.replace('*', '')
+                    print(size)
+                    #Loop through carrier options
+                    if country == 'US': #trade in and payment
+                        need_trade_pay = True
+                        for k in range(carrier_length):
+                            click_element(driver,'//*[@id="Item3"]/div/fieldset/div[1]/div[2]/div['+ str(k+1) +']')
+                            carrier = driver.find_element_by_xpath('//*[@id="Item3"]/div/fieldset/div[1]/div[2]/div['+ str(k+1) +']/div/label/span[2]').text
+                            print(carrier)
+                            if need_trade_pay:
+                                click_element(driver,'//*[@id="tradeup-inline-heroselector"]/div/div/fieldset/div/div[1]')
+                                time.sleep(delay)
+                                click_element(driver,'//*[@id="primary"]/materializer/purchase-options/fieldset/materializer[1]/div/div[1]')
+                                time.sleep(delay)
+                                need_trade_pay = False
+                            date = driver.find_element_by_xpath('//*[@id="primary"]/summary-builder/div[2]/div[1]/materializer/div[2]/div/div/ul/li/span').text
+                            save_record(color, size, country, carrier, date, date_format)
+                    elif country == 'UK': #trade in and payment
+                        need_trade_pay = True
                         if need_trade_pay:
-                            #Click "No" for trade in
-                            trade_element = driver.find_element_by_xpath('//*[@id="tradeup-inline-heroselector"]/div/div/fieldset/div/div[1]')
-                            driver.execute_script("arguments[0].scrollIntoView();", trade_element)
-                            trade_element.click()
-                            time.sleep(delay_long)
-                            #Click one-time payment
-                            payment_element = driver.find_element_by_xpath('//*[@id="primary"]/materializer/purchase-options/fieldset/materializer[1]/div/div[1]')
-                            driver.execute_script("arguments[0].scrollIntoView();", payment_element)
-                            payment_element.click()
-                            time.sleep(delay_long)
+                            click_element(driver,'//*[@id="tradeup-inline-heroselector"]/div/div/fieldset/div/div[1]/div/div')
+                            time.sleep(delay)
+                            click_element(driver,'//*[@id="primary"]/materializer/purchase-options/fieldset/materializer[2]/div/div[2]/div/div')
+                            time.sleep(delay)
                             need_trade_pay = False
                         date = driver.find_element_by_xpath('//*[@id="primary"]/summary-builder/div[2]/div[1]/materializer/div[2]/div/div/ul/li/span').text
-                        save_record(color, size, country, carrier, date, date_format)
-                elif country == 'UK':
-                    need_trade_pay = True
-                    if need_trade_pay:
-                        #Click "No" for trade in
-                        trade_element = driver.find_element_by_xpath('//*[@id="tradeup-inline-heroselector"]/div/div/fieldset/div/div[1]/div/div')
-                        driver.execute_script("window.scrollTo(0, 0);", trade_element)
-                        trade_element.click()
+                        save_record(color, size, country, None, date, date_format)
+                    elif country == 'Germany': #payment
+                        need_trade_pay = True
+                        if need_trade_pay:
+                            click_element(driver,'//*[@id="primary"]/materializer/purchase-options/fieldset/materializer[2]/div/div[2]/div/div')
+                            time.sleep(delay)
+                            need_trade_pay = False
+                        date = driver.find_element_by_xpath('//*[@id="primary"]/summary-builder/div[2]/div[1]/materializer/div[2]/div/div/ul/li/span').text
+                        save_record(color, size, country, None, date, date_format)
+                    elif country == 'France' or country == 'Japan': #trade in
+                        need_trade_pay = True
+                        if need_trade_pay:
+                            click_element(driver,'//*[@id="tradeup-inline-heroselector"]/div/div/fieldset/div/div[1]/div/div')
+                            time.sleep(delay)
+                            need_trade_pay = False
+                        date = driver.find_element_by_xpath('//*[@id="primary"]/summary-builder/div[2]/div[1]/materializer/div[2]/div/div/ul/li/span').text
+                        save_record(color, size, country, None, date, date_format)
+                    else:
                         time.sleep(delay_long)
-                        #Click one-time payment
-                        payment_element = driver.find_element_by_xpath('//*[@id="primary"]/materializer/purchase-options/fieldset/materializer[2]/div/div[2]/div/div')
-                        driver.execute_script("window.scrollTo(0, 0);", payment_element)
-                        payment_element.click()
-                        time.sleep(delay_long)
-                        need_trade_pay = False
-                    date = driver.find_element_by_xpath('//*[@id="primary"]/summary-builder/div[2]/div[1]/materializer/div[2]/div/div/ul/li/span').text
-                    save_record(color, size, country, None, date, date_format)
-                elif country == 'Germany':
-                    need_trade_pay = True
-                    if need_trade_pay:
-                        #Click one-time payment
-                        payment_element = driver.find_element_by_xpath('//*[@id="primary"]/materializer/purchase-options/fieldset/materializer[2]/div/div[2]/div/div')
-                        driver.execute_script("window.scrollTo(0, 0);", payment_element)
-                        payment_element.click()
-                        time.sleep(delay_long)
-                        need_trade_pay = False
-                    date = driver.find_element_by_xpath('//*[@id="primary"]/summary-builder/div[2]/div[1]/materializer/div[2]/div/div/ul/li/span').text
-                    save_record(color, size, country, None, date, date_format)
-                elif country == 'France' or country == 'Japan':
-                    need_trade_pay = True
-                    if need_trade_pay:
-                        #Click "No" for trade in
-                        trade_element = driver.find_element_by_xpath('//*[@id="tradeup-inline-heroselector"]/div/div/fieldset/div/div[1]/div/div')
-                        driver.execute_script("window.scrollTo(0, 0);", trade_element)
-                        trade_element.click()
-                        time.sleep(delay_long)
-                        need_trade_pay = False
-                    date = driver.find_element_by_xpath('//*[@id="primary"]/summary-builder/div[2]/div[1]/materializer/div[2]/div/div/ul/li/span').text
-                    save_record(color, size, country, None, date, date_format)
-                else:
-                    time.sleep(delay_long)
-                    date = driver.find_element_by_xpath('//*[@id="primary"]/summary-builder/div[2]/div[1]/materializer/div[2]/div/div/ul/li/span').text
-                    save_record(color, size, country, None, date, date_format)
-            print('-----------------------------')
-except Exception as e:
-    print("ERROR! did not complete scraping")
-    print(e)
-    traceback.print_exc()
-finally:
-    completion_time = datetime.now() - start_time
-    print(f'Time to complete (seconds): {completion_time.seconds}')
-    #Store records into CSV file
-    with open('data.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Color', 'Size', 'Country', 'Carrier', 'Date Delivered', 'Number of Days to Ship'])
-        for record in records:
-            writer.writerow([record.color, record.size, record.country, record.carrier, record.date, record.day_to_ship])
+                        date = driver.find_element_by_xpath('//*[@id="primary"]/summary-builder/div[2]/div[1]/materializer/div[2]/div/div/ul/li/span').text
+                        save_record(color, size, country, None, date, date_format)
+                print('-----------------------------')
+    except Exception as e:
+        print("ERROR! did not complete scraping")
+        print(e)
+        traceback.print_exc()
+    finally:
+        completion_time = datetime.now() - start_time
+        print(f'Time to complete (seconds): {completion_time.seconds}')
+        #Store records into CSV file
+        with open('data.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Color', 'Size', 'Country', 'Carrier', 'Date Delivered', 'Number of Days to Ship'])
+            for record in records:
+                writer.writerow([record.color, record.size, record.country, record.carrier, record.date, record.day_to_ship])
+
+execute_scrape(countries)
+
+
 
